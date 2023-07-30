f41e86dd5743f4ddb72fda266763436b1f6dde04
Ivan Silva <silvaivan280@gmail.com>
2023-07-30 20:36:03 -0300

    agregue la ruta para los metodos get, delete y put de productos por su id

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { borrarProducto, crearProducto, editarProducto, obtenerListaProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
-router.route('/productos')
+router.route("/productos").post(crearProducto).get(obtenerListaProductos);
+router.route("/productos/:id").get(obtenerListaProductos).delete(borrarProducto).put(editarProducto);
 
 
 export default router;

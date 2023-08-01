76ea498ad615fd6042b4a014584e533d565628e0
belen ramos <ramos.emiliabelen@gmail.com>
2023-08-01 18:45:33 -0300

    se creo la ruta de pedido pendiente a entregado

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -4,4 +4,5 @@ import { crearPedido } from "../controllers/pedidos.controllers";
 const router = Router();
 
 router.route("/").post(crearPedido)
+router.route("/entregado").put(entregarPedido)
 export default router;

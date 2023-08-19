3815eed96331ee8368e87e855a42b8cc9bee0773
belen ramos <ramos.emiliabelen@gmail.com>
2023-08-19 14:28:59 -0300

    se realizo la rutacontrolador de la consulta de un pedido

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { consultaEntregarPedido, consultaPedidoPendiente, crearPedido, listaPedidos } from "../controllers/pedidos.controllers";
+import { consultaEntregarPedido, consultaPedidoPendiente, crearPedido, listaPedidos, consultaPedido } from "../controllers/pedidos.controllers";
 
 const router = Router();
 
 router.route("/").post(crearPedido).get(listaPedidos)
 router.route("/entregado/:id").put(consultaEntregarPedido)
 router.route("/pendiente/:id").put(consultaPedidoPendiente)
+router.route("/:id").get(consultaPedido)
 
 export default router;

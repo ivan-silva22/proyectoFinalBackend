fd20fa2dde76ea4b1f4bd7d909002ff769787ecd
belen ramos <ramos.emiliabelen@gmail.com>
2024-02-03 17:46:12 -0300

    se creo la ruta de login del usuario. Se termino el login usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { crearUsuario, obtenerUsuario, obtenerUsuarios } from "../controllers/usuario.controllers";
+import { crearUsuario, login, obtenerUsuario, obtenerUsuarios } from "../controllers/usuario.controllers";
 
 const router = Router();
 
 router.route("/usuarios").post(crearUsuario).get(obtenerUsuarios);
 router.route("/usuarios/:id").get(obtenerUsuario);
+router.route("/usuarios/login").post(login)
 
 
 export default router;
